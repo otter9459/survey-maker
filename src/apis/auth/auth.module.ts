f459498943989d94adfc8f102b56784a06f7e9ea
이정훈 <ijeonghun@ijeonghun-ui-MacBookPro.local>
2023-11-19 20:17:47 +0900

    :heavy_plus_sign: Auth module - 의존성 주입

--- src/apis/auth/auth.module.ts
+++ src/apis/auth/auth.module.ts
@@ -3,12 +3,17 @@ import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { AdminModule } from '../admin/admin.module';
+import { JwtAdminStrategy } from './strategies/jwt-admin.strategy';
 
 @Module({
   imports: [
     JwtModule.register({}), //
     AdminModule,
   ],
-  providers: [AuthResolver, AuthService],
+  providers: [
+    AuthResolver, //
+    AuthService,
+    JwtAdminStrategy,
+  ],
 })
 export class AuthModule {}
