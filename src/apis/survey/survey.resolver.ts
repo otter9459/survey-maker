242b89ca28358fc9ae5f9a40b96e3d6854f57fc7
이정훈 <ijeonghun@ijeonghun-ui-MacBookPro.local>
2023-11-19 21:08:47 +0900

    :hammer: Survey module - createSurvey API resolver 제작

--- src/apis/survey/survey.resolver.ts
+++ src/apis/survey/survey.resolver.ts
@@ -1,4 +1,22 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { SurveyService } from './survey.service';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+import { Survey } from './entity/survey.entity';
+import { IContext } from 'src/commons/interfaces/context';
 
 @Resolver()
-export class SurveyResolver {}
+export class SurveyResolver {
+  constructor(
+    private readonly surveyService: SurveyService, //
+  ) {}
+
+  @UseGuards(GqlAuthGuard('admin'))
+  @Mutation(() => Survey)
+  async createSurvey(
+    @Context() context: IContext, //
+    @Args('createSurveyInput') createSurveyInput: CreateSurveyInput,
+  ): Promise<Survey> {
+    return this.surveyService.create({ context, createSurveyInput });
+  }
+}
