26e884a37c49c11b832b9354498e3a171bbfbcc4
이정훈 <ijeonghun@ijeonghun-ui-MacBookPro.local>
2023-11-20 18:01:54 +0900

    :hammer: User module - fetchUser API resolver 작성

--- src/apis/user/user.resolver.ts
+++ src/apis/user/user.resolver.ts
@@ -1,13 +1,24 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { User } from './entity/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+import { IContext } from 'src/commons/interfaces/context';
 
 @Resolver()
 export class UserResolver {
   constructor(
     private readonly userService: UserService, //
   ) {}
+
+  @UseGuards(GqlAuthGuard('access'))
+  @Query(() => User)
+  async fetchUser(
+    @Context() context: IContext, //
+  ): Promise<User> {
+    return this.userService.findOne({ id: context.req.user.id });
+  }
 
   @Mutation(() => User)
   async createUser(
