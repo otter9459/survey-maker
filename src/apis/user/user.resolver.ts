bda7c7a1b25f59b7eb934ae9c917b668441f1a26
이정훈 <ijeonghun@ijeonghun-ui-MacBookPro.local>
2023-11-20 17:25:00 +0900

    :hammer: User module - createUser API resolver 작성

--- src/apis/user/user.resolver.ts
+++ src/apis/user/user.resolver.ts
@@ -1,4 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { User } from './entity/user.entity';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(
+    private readonly userService: UserService, //
+  ) {}
+
+  @Mutation(() => User)
+  async createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput, //
+  ): Promise<User> {
+    return this.userService.create({ createUserInput });
+  }
+}
