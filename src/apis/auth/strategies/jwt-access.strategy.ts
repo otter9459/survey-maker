a6fc0196b7a56ca9db8901dce1247a80bd5526f9
이정훈 <ijeonghun@ijeonghun-ui-MacBookPro.local>
2023-11-20 17:59:46 +0900

    :hammer: Auth module - JwtAccessStrategy 작성

---
+++ src/apis/auth/strategies/jwt-access.strategy.ts
@@ -0,0 +1,21 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+export class JwtAdminStrategy extends PassportStrategy(Strategy, 'access') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_ACCESS_KEY,
+    });
+  }
+
+  async validate(payload) {
+    try {
+      return {
+        id: payload.sub,
+      };
+    } catch (e) {
+      throw e;
+    }
+  }
+}
