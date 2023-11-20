67ad74056399db0363919432905ab5339749cae4
이정훈 <ijeonghun@ijeonghun-ui-MacBookPro.local>
2023-11-20 18:26:38 +0900

    :sparkles: User module - updateUserPassword API 완성

--- src/apis/user/user.service.ts
+++ src/apis/user/user.service.ts
@@ -1,4 +1,9 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { Repository } from 'typeorm';
@@ -44,6 +49,32 @@ export class UserService {
     const result = await this.userRepository.update(
       { id: userId },
       { ...updateUserInput },
+    );
+
+    return result.affected ? true : false;
+  }
+
+  async updatePassword({
+    email,
+    currentPassword,
+    newPassword,
+  }): Promise<boolean> {
+    const user = await this.findOneByEmail({ email });
+    if (!user) throw new NotFoundException('존재하지 않는 사용자 정보입니다.');
+
+    const isCorrect = await bcrypt.compare(currentPassword, user.password);
+    if (!isCorrect) throw new BadRequestException('기존 암호가 틀립니다.');
+
+    const isSame = await bcrypt.compare(newPassword, user.password);
+    if (isSame)
+      throw new BadRequestException(
+        '기존 암호와 같은 암호로는 변경할 수 없습니다.',
+      );
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    const result = await this.userRepository.update(
+      { id: user.id },
+      { password: hashedPassword },
     );
 
     return result.affected ? true : false;
