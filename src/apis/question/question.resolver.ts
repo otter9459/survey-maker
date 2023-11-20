560c1ab76728eda6fa198f946e326c0b45826bae
이정훈 <ijeonghun@ijeonghun-ui-MacBookPro.local>
2023-11-20 12:48:38 +0900

    :hammer: Question module - create API resolver 작성

--- src/apis/question/question.resolver.ts
+++ src/apis/question/question.resolver.ts
@@ -1,4 +1,25 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Resolver } from '@nestjs/graphql';
+import { QuestionService } from './question.service';
+import { Question } from './entity/question.entity';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+import { IContext } from 'src/commons/interfaces/context';
 
 @Resolver()
-export class QuestionResolver {}
+export class QuestionResolver {
+  constructor(
+    private readonly questionService: QuestionService, //
+  ) {}
+
+  @UseGuards(GqlAuthGuard('admin'))
+  @Mutation(() => Question)
+  async createQuestion(
+    @Context() context: IContext, //
+    @Args('createQuestionInput') createQuestionInput: CreateQuestionInput,
+  ) {
+    return this.questionService.create({
+      adminId: context.req.user.id,
+      createQuestionInput,
+    });
+  }
+}
