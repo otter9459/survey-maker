ca701cbb02001bd66b8bd09c948d41020e3dd04f
이정훈 <ijeonghun@ijeonghun-ui-MacBookPro.local>
2023-11-20 19:59:06 +0900

    :hammer: Response module - createResponse API resolver 작성

--- src/apis/response/response.resolver.ts
+++ src/apis/response/response.resolver.ts
@@ -1,4 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { ResponseService } from './response.service';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+import { IContext } from 'src/commons/interfaces/context';
 
 @Resolver()
-export class ResponseResolver {}
+export class ResponseResolver {
+  constructor(
+    private readonly responseService: ResponseService, //
+  ) {}
+
+  @UseGuards(GqlAuthGuard('access'))
+  @Mutation(() => Boolean)
+  async createResponse(
+    @Context() context: IContext,
+    @Args('createResponseInput') createResponseInput: CreateResponseInput,
+  ): Promise<boolean> {
+    return this.responseService.create({
+      userId: context.req.user.id,
+      createResponseInput,
+    });
+  }
+}
